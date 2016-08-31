f8f8a30c94b7806241ed90669b1f1a1c39464b50
yuerzx <hansun@1230.me>
2016-08-31 17:18:05 +1000

    Initial commit

---
+++ src/reducer/reduer_books.js
@@ -0,0 +1,14 @@
+/**
+ * Created by yuerzx on 31/08/2016.
+ */
+
+export default function(){
+    return [
+        {title: "Jas"},
+        {title: "Harry Potter"},
+        {
+            title: "New Start"
+        }
+    ]
+}
+
